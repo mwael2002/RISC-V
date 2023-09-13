8bcb690ba9afd65e472e8fe7ddeb2b828c1afb77
Mohammad Wael <102371006+mwael2002@users.noreply.github.com>
2023-09-13 06:50:14 +0300

    Update Register_File.v

--- Register_File.v
+++ Register_File.v
@@ -1,10 +1,10 @@
-module Register_File(Read_Register1,Read_Register2,Write_Register,Write_Data,Write_Enable,clk,areset,
+module Register_File(Read_Register1,Read_Register2,Write_Register,Write_Data,Write_Enable,clk,
 Read_Data1,Read_Data2);
 
 
 input [4:0] Read_Register1,Read_Register2,Write_Register;
 input [31:0] Write_Data;
-input Write_Enable,clk,areset;
+input Write_Enable,clk;
 output [31:0] Read_Data1,Read_Data2;
  
 reg [31:0] RF [0:31];
@@ -12,25 +12,11 @@ reg [31:0] RF [0:31];
 
 integer counter;
 
-always @(posedge clk or negedge areset)
+always @(posedge clk)
 begin
 
-if(!areset)
-begin
-for(counter=0;counter<32;counter=counter+1)
-begin
-
-RF[counter]<=0;
-
-end
-end
-else if(Write_Enable)
-begin
-if(Write_Register!=0)
-begin
+if(Write_Enable)
 RF[Write_Register]<=Write_Data;
-end
-end
 
 end
 
