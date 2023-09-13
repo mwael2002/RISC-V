1709e61f0ab4c02c2cee723469214260f69ebed8
Mohammad Wael <102371006+mwael2002@users.noreply.github.com>
2023-09-13 05:10:29 +0300

    Update RISC_V.v

--- RISC_V.v
+++ RISC_V.v
@@ -33,7 +33,7 @@ MUX_32 MUX0(.IN0(SrcC),.IN1(ImmExt),.sel(Control_signals[4]),.OUT(SrcB));
 ALU_32_bit ALU0(.A(SrcA),.B(SrcB),.opcode(ALUControl),
 .OUT(ALUResult),.zero_flag(zero_flag),.sign_flag(sign_flag));
 
-Data_memory RAM(.Address(ALUResult>>2),.Write_Data(SrcC),.Write_Enable(Control_signals[3]),.clk(clk),
+Data_memory RAM(.Address(ALUResult[31:2]),.Write_Data(SrcC),.Write_Enable(Control_signals[3]),.clk(clk),
 .Read_Data(Read_Data));
 
 
